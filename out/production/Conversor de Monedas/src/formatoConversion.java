85ef1ff9b7011429dc5b88ac56f620af81a4fc87
diavels <fabian.vigorena873@gmail.com>
2025-05-11 23:52:27 -0400

    up proyecto

---
+++ out/production/Conversor de Monedas/src/formatoConversion.java
@@ -0,0 +1,39 @@
+public class formatoConversion {
+
+    private String monedaBase;
+    private String monedaCambio;
+    private double valorMoneda;
+    private String ultimaActualizacion;
+
+    public formatoConversion(valoresJson formato) {
+        this.monedaBase = formato.base_code();
+        this.monedaCambio = formato.target_code();
+        this.valorMoneda = formato.conversion_rate();
+        this.ultimaActualizacion = formato.time_last_update_utc();
+    }
+
+    public String getUltimaActualizacion() {
+        return ultimaActualizacion;
+    }
+
+    public double isValorMoneda() {
+        return valorMoneda;
+    }
+
+    public String getMonedaCambio() {
+        return monedaCambio;
+    }
+
+    public String getMonedaBase() {
+        return monedaBase;
+    }
+
+    @Override
+    public String toString() {
+        return "(Informacion de la Conversion: " +
+               "monedaBase= " + monedaBase +
+               ", monedaCambio= " + monedaCambio +
+               ", valorMoneda= " + valorMoneda +
+               ", ultimaActualizacion= " + ultimaActualizacion + ")";
+    }
+}
